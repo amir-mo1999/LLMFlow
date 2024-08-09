547b61963e7a508ed9985ff187ad9eb2930f1b60
amir-mo1999 <amir.mohammad@hotmail.de>
2024-08-09 04:24:42 +0200

    implemented first example with promptfoo

--- promptfoo-server/src/app.js
+++ promptfoo-server/src/app.js
@@ -1,11 +1,38 @@
 import express from "express";
 import "dotenv/config";
+import promptfoo from "promptfoo";
 
 const app = express();
 const port = parseInt(process.env.PORT);
 
-app.get("/", (req, res) => {
-  res.send("gegewg");
+app.get("/", async (req, res) => {
+  const results = await promptfoo.evaluate(
+    {
+      prompts: [
+        "Rephrase this in French: {{body}}",
+        "Rephrase this like a pirate: {{body}}",
+      ],
+      providers: ["openai:gpt-4o-mini"],
+      tests: [
+        {
+          vars: {
+            body: "Hello world",
+          },
+        },
+        {
+          vars: {
+            body: "I'm hungry",
+          },
+        },
+      ],
+      writeLatestResults: true, // write results to disk so they can be viewed in web viewer
+    },
+    {
+      maxConcurrency: 2,
+    }
+  );
+
+  res.send(results);
 });
 
 app.listen(port, () => {
