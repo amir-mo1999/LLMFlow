fc73ba16096dc0018dd9638f2be10c48ad947d59
amir-mo1999 <amir.mohammad@hotmail.de>
2024-08-15 16:30:31 +0200

    added alias for _type field

--- server/App/models/output_assertion.py
+++ server/App/models/output_assertion.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, confloat
+from pydantic import BaseModel, confloat, Field
 from typing import Optional, Literal, Annotated
 
 
 class OutputAssertion(BaseModel):
-    type: Literal["contains", "contains-sql", ""]
+    _type: Literal["contains", "contains-sql", ""] = Field(..., alias="type")
     value: Optional[str]
     weight: Annotated[float, confloat(ge=0.05, le=1)]
