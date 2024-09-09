d0030ca4e9355f0cd973d304d83927308344d0cf
amir-mo1999 <amir.mohammad@hotmail.de>
2024-09-09 03:07:06 +0200

    moved

--- server/App/models/ai_function.py
+++ server/App/models/ai_function.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Annotated, Dict, List
+from typing import Annotated, Dict, List, Literal, Optional
 
 from pydantic import (
     BaseModel,
@@ -7,11 +7,11 @@ from pydantic import (
     EmailStr,
     Field,
     NonNegativeInt,
+    confloat,
     StringConstraints,
 )
 
 from .objectID import PydanticObjectId
-from .output_assertion import OutputAssertions
 
 
 class InputVariable(BaseModel):
@@ -20,6 +20,19 @@ class InputVariable(BaseModel):
 
 class TestCase(BaseModel):
     variables: Dict[str, str] = Field(..., alias="vars")
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class OutputAssertion(BaseModel):
+    assertion_type: Literal["contains", "contains-sql", ""] = Field(..., alias="type")
+    value: Optional[str]
+    weight: Annotated[float, confloat(ge=0.05, le=1)] = 1
+
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class OutputAssertions(BaseModel):
+    assertions: List[OutputAssertion] = Field(..., alias="assert")
     model_config = ConfigDict(populate_by_name=True)
 
 
