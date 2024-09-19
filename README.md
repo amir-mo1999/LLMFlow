bcc1a4086ba1e2fa3c8ebc95c5c6fbf62d5fbe8d
amir-mo1999 <amir.mohammad@hotmail.de>
2024-09-19 14:46:21 +0200

    added codegen

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-To generate the client bindings run the following:
-`npx swagger-typescript-api -p http://localhost:4000/openapi.json  -n ./client/src/api.ts`
+npx openapi-codegen gen api
---
+++ client/openapi-codegen.config.ts
@@ -0,0 +1,24 @@
+import {
+  generateSchemaTypes,
+  generateReactQueryComponents,
+} from "@openapi-codegen/typescript";
+import { defineConfig } from "@openapi-codegen/cli";
+export default defineConfig({
+  api: {
+    from: {
+      source: "url",
+      url: "http://localhost:4000/openapi.json",
+    },
+    outputDir: "src/api",
+    to: async (context) => {
+      const filenamePrefix = "api";
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix,
+      });
+      await generateReactQueryComponents(context, {
+        filenamePrefix,
+        schemasFiles,
+      });
+    },
+  },
+});
