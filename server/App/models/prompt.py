5ed5ccabf6ce721bf409bb2a39e43846a9bef17f
amir-mo1999 <amir.mohammad@hotmail.de>
2024-09-29 02:38:12 +0200

    fixed examples

--- server/App/models/prompt.py
+++ server/App/models/prompt.py
@@ -18,7 +18,7 @@ class PromptRouteInput(RootModel):
         example=[
             PromptMessage(
                 role="user",
-                content="Summarize the following text: {{text}} in {{number of sentences}} sentences.",
+                content="Summarize the following text: {{text}} in {{number_of_sentences}} sentences.",
             )
         ],
     )
