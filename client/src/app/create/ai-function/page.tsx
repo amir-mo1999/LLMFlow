2c41abf808ba6fcc001ba8ac008bff159dad9e75
amir-mo1999 <amir.mohammad@hotmail.de>
2024-10-23 23:29:02 +0200

    remove redundant pages

--- client/src/app/create/ai-function/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { AIFunctionForm } from "@/components"
-
-export default function Home() {
-  return <AIFunctionForm></AIFunctionForm>
-}
--- client/src/app/create/prompt/[aiFunctionID]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PromptForm } from "@/components/Prompt"
-
-export default function Page({ params }: { params: { aiFunctionID: string } }) {
-  return <PromptForm aiFunctionID={params.aiFunctionID}></PromptForm>
-}
