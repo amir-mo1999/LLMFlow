97134629fb100195f273e8a35f57fefe64393320
amir-mo1999 <amir.mohammad@hotmail.de>
2024-11-08 00:53:58 +0100

    pre commit

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,19 +1,21 @@
+import os
+
 import uvicorn
 from dotenv import load_dotenv
-import os
+
 load_dotenv()
 
 MODE = os.getenv("MODE")
-HOST = "0.0.0.0"
+host = "0.0.0.0"
 
 if MODE == "PROD":
-    HOST = "::"
+    host = "::"
 
 # entry point to fastAPI application
 if __name__ == "__main__":
     uvicorn.run(
         "App.__init__:app",
-        host=HOST,
+        host=host,
         port=4000,
         reload=True,
         reload_dirs=["/App", "/home/appuser", "App/"],
