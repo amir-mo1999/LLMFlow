342f1975cd305d8742b6316da3bed7d7e6adc715
amir-mo1999 <amir.mohammad@hotmail.de>
2024-11-07 23:28:18 +0100

    enable ipv6

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,13 +1,19 @@
 import uvicorn
 from dotenv import load_dotenv
-
+import os
 load_dotenv()
+
+MODE = os.getenv("MODE")
+HOST = "0.0.0.0"
+
+if MODE == "PROD":
+    HOST = "::"
 
 # entry point to fastAPI application
 if __name__ == "__main__":
     uvicorn.run(
         "App.__init__:app",
-        host="0.0.0.0",
+        host=HOST,
         port=4000,
         reload=True,
         reload_dirs=["/App", "/home/appuser", "App/"],
