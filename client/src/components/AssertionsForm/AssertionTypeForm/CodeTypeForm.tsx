042acae4bd10511aa1431441b940dd7ae6900ba6
amir-mo1999 <amir.mohammad@hotmail.de>
2024-11-12 00:55:32 +0100

    improved hints

--- client/src/components/AssertionsForm/AssertionTypeForm/CodeTypeForm.tsx
+++ client/src/components/AssertionsForm/AssertionTypeForm/CodeTypeForm.tsx
@@ -13,8 +13,14 @@ interface CodeTypeFormProps {
 }
 
 const CodeTypeForm: React.FC<CodeTypeFormProps> = ({ value, setValue, extension, sx }) => {
-  const pythonDefault = `# Implement your assertion logic as a script that returns 'True' if the result passed the assertion and 'False' otherwise
-# the 'output' variable is injected which holds the Provider response as a string
+  const pythonDefault = `# Implement your assertion logic as a script that returns 'True' if the result passed the assertion and 'False' otherwise.
+# You may also return an instance of the "GradingResult" object
+# "output" is the raw string response of the Provider, "context" holds test case data and is an AssertContext instance
+# class AssertContext(TypedDict):
+#   prompt: str
+#    vars: Dict[str, str] # Contains input variables as keys and their content as values
+#    test: Dict[str, Any] # Contains keys like "vars", "assert", "options"
+
 if output:
     return True
 else:
@@ -22,12 +28,20 @@ else:
 `
 
   const javascriptDefault = `// Implement your assertion logic as a script that returns 'true' if the result passed the assertion and 'false' otherwise
-// the 'output' variable is injected which holds the Provider response as a string
+// 'output' holds the raw string response of the Provider and 'context' holds the raw prompt and the test case variables and is of type AssertContext
+
+// interface AssertContext {
+//    Raw prompt sent to LLM
+//    prompt: string;
+//    Test case variables
+//  vars: Record<string, string | object>;
+//  }
+
 if (output) {
-  return true
-} else {
-  return false
-}
+    return true
+  } else {
+    return false
+  }
 `
   const [defaultVal, setDefaultVal] = useState(
     extension === "python" ? pythonDefault : extension === "javascript" ? javascriptDefault : ""
