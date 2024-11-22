2e5065960686a3c6a4702baca7919fc1c9bcdff9
amir-mo1999 <amir.mohammad@hotmail.de>
2024-11-22 13:13:53 +0100

    added feature to download openapi specifcation for a project

--- client/src/app/(app)/projects/[projectID]/docs/page.tsx
+++ client/src/app/(app)/projects/[projectID]/docs/page.tsx
@@ -2,19 +2,25 @@
 import { useGetProjectApiDocs } from "@/api/apiComponents"
 import Typography from "@mui/material/Typography"
 import Box from "@mui/material/Box"
-import { useState } from "react"
+import { useState, useContext } from "react"
 import Button from "@mui/material/Button"
 import Snackbar from "@mui/material/Snackbar"
 import Alert from "@mui/material/Alert"
+import DownloadIcon from "@mui/icons-material/Download"
+import { ProjectContext } from "@/contexts"
 
 import ContentCopyIcon from "@mui/icons-material/ContentCopy"
 
 export default function Page({ params }: { params: { projectID: string } }) {
-  const [copySuccess, setCopySuccess] = useState(false)
+  const { projects } = useContext(ProjectContext)
+  const project = projects.find((project) => project._id === params.projectID)
 
   const { data: apiDocs } = useGetProjectApiDocs({
     pathParams: { projectId: params.projectID },
   })
+
+  const [copySuccess, setCopySuccess] = useState(false)
+
   const handleCopy = async () => {
     try {
       await navigator.clipboard.writeText(JSON.stringify(apiDocs, null, 2))
@@ -27,14 +33,35 @@ export default function Page({ params }: { params: { projectID: string } }) {
     setCopySuccess(false)
   }
 
-  if (!apiDocs) return <></>
+  const downloadApiSpec = () => {
+    const jsonString = JSON.stringify(apiDocs, null, 2)
+    const blob = new Blob([jsonString], { type: "application/json" })
+    const url = URL.createObjectURL(blob)
+
+    const link = document.createElement("a")
+    link.href = url
+    link.download = `${project?.name.replace(" ", "-").toLowerCase()}-api-spec.json`
+
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+
+    URL.revokeObjectURL(url)
+  }
+
+  if (!apiDocs || !project) return <></>
 
   return (
     <>
       <Box display="flex" flexDirection="column" alignItems="start" width="100%" gap={3}>
-        <Button variant="contained" onClick={handleCopy}>
-          <ContentCopyIcon />
-        </Button>
+        <Box display="flex" alignItems="center" gap={2}>
+          <Button variant="contained" onClick={handleCopy}>
+            <ContentCopyIcon />
+          </Button>
+          <Button variant="contained" onClick={downloadApiSpec}>
+            <DownloadIcon />
+          </Button>
+        </Box>
         <Typography>{JSON.stringify(apiDocs, null, 2)}</Typography>
       </Box>
       <Snackbar
